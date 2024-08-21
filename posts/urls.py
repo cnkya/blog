bdd4f168411c9deacb2886aea0a3563593c2568e
Christina Nkya <christina.nkya@sdgku.edu>
2024-08-21 13:37:45 -0400

    Fixed minor issues and adjusted css

--- posts/urls.py
+++ posts/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path("new/", views.PostCreateView.as_view(), name="new"),
     path("<int:pk>/update/", views.PostUpdateView.as_view(), name="update"),
     path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),
-    path("archived/", views.DraftPostListView.as_view(), name="archived"),
+    path("draft/", views.DraftPostListView.as_view(), name="draft"),
+    path("archived/", views.ArchivedPostListView.as_view(), name="archived"),
     
     
 ]
